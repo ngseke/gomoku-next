cff2a5ee3e058c562edc9f4ce32112ae823cf044
ngseke <ngseke@gmail.com>
2024-04-20 16:19:27 +0800

    feat: update style

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,15 +1,19 @@
 import { type ComponentProps } from 'react'
 
-export function Input (props: ComponentProps<'input'>) {
+export function Input ({
+  placeholder,
+  ...restProps
+}: ComponentProps<'input'>) {
   return (
     <div className="relative h-10 w-full min-w-0">
       <input
         className="peer size-full rounded-lg border border-neutral-200 bg-gradient-to-br from-[#fdfbfb] from-50% to-[#ebedee] px-2 text-lg outline outline-1 outline-transparent duration-150 focus:outline-[#fcb69f]"
+        placeholder={placeholder ?? 'Aa'}
         type="text"
-        {...props}
+        {...restProps}
       />
       <span
-        className="absolute inset-0 -z-10 rounded-lg bg-gradient-to-br from-[#ffecd2] to-[#fcb69f] opacity-0 blur-lg transition-opacity duration-200 peer-focus:opacity-70"
+        className="absolute inset-0 -z-10 rounded-lg bg-gradient-to-br from-[#ffecd2] to-[#fcb69f] opacity-0 blur-lg transition-opacity duration-200 peer-focus:opacity-50"
       />
     </div>
   )
--- components/RoundButton.tsx
+++ components/RoundButton.tsx
@@ -5,16 +5,22 @@ export function RoundButton ({ icon, loading, children, ...restProps }: Componen
   loading?: boolean
 }) {
   return (
-    <button
-      className="inline-flex h-10 min-w-20 items-center justify-center gap-2 rounded-full border border-neutral-200 px-3 text-sm font-bold text-neutral-800 duration-200 hover:bg-neutral-200 bg-neutral-100"
-      type="button"
-      {...restProps}
-    >
-      {icon && <span className="text-base">{icon}</span>}
+    <div className="relative h-10 min-w-20">
+      <button
+        className="peer inline-flex size-full items-center justify-center gap-2 rounded-full border border-neutral-200 bg-neutral-100 px-3 text-sm font-bold text-neutral-800 outline outline-1 outline-transparent duration-150 active:scale-[97%]"
+        type="button"
+        {...restProps}
+      >
+        {icon && <span className="text-base">{icon}</span>}
 
-      <span className="shrink-0">
-        {children}
-      </span>
-    </button>
+        <span className="shrink-0">
+          {children}
+        </span>
+      </button>
+      <span
+        className="absolute inset-0 -z-10 rounded-lg bg-gradient-to-br from-neutral-200 to-neutral-300 opacity-0 blur-lg transition-opacity duration-200 peer-hover:opacity-70"
+      />
+    </div>
+
   )
 }
