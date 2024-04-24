18cc31e459c75b576a1e246b66fc57f0b5333834
ngseke <ngseke@gmail.com>
2024-04-24 21:46:14 +0800

    feat: enlarge piece

--- components/GomokuBoard/Piece.tsx
+++ components/GomokuBoard/Piece.tsx
@@ -6,7 +6,7 @@ export function Piece ({
 }: { color: 'black' | 'white' }) {
   return (
     <div
-      className={cn('size-9/12 rounded-full border border-transparent bg-gradient-to-tr from-neutral-200 to-neutral-200', {
+      className={cn('size-10/12 rounded-full border border-transparent bg-gradient-to-tr from-neutral-200 to-neutral-200', {
         'from-[#000] to-[#434343] bg-gradient-to-tr border-neutral-700/50': color === 'black',
         'from-[#cfd4d7] to-[#fdfbfb] border-neutral-400/50': color === 'white',
       })}
@@ -17,7 +17,7 @@ export function Piece ({
 export function PieceGhost ({ className }: ComponentProps<'div'>) {
   return (
     <div
-      className={cn('absolute size-9/12 rounded-full border-2 border-dotted border-[#fcb69f]/50 bg-[#fcb69f]/10 duration-100', className)}
+      className={cn('absolute size-10/12 rounded-full border-2 border-dotted border-[#fcb69f]/50 bg-[#fcb69f]/10 duration-100', className)}
     />
   )
 }
