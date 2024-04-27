8f3064d6f2fa1c60d0268cdbbf012a7d60a51ce1
ngseke <ngseke@gmail.com>
2024-04-27 18:23:18 +0800

    docs: add comment

--- hooks/usePlayerStateStore.ts
+++ hooks/usePlayerStateStore.ts
@@ -1,5 +1,6 @@
 import { useAppSelector } from '@/lib/hooks'
 
+/** The player state that syncs with Firebase in realtime */
 export function usePlayerStateStore () {
   return useAppSelector((state) => state.game.playerState)
 }
--- hooks/useRoomStore.ts
+++ hooks/useRoomStore.ts
@@ -1,5 +1,6 @@
 import { useAppSelector } from '@/lib/hooks'
 
+/** The room state that syncs with Firebase in realtime */
 export function useRoomStore () {
   return useAppSelector((state) => state.game.room)
 }
