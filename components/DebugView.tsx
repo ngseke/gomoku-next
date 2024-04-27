4ac77ca45d690a5ba8082c974fd7a57255dfcdae
ngseke <ngseke@gmail.com>
2024-04-27 19:09:12 +0800

    feat: show session id

--- components/DebugView.tsx
+++ components/DebugView.tsx
@@ -1,3 +1,4 @@
+import { useAuthStore } from '@/hooks/useAuthStore'
 import { usePlayerStateStore } from '@/hooks/usePlayerStateStore'
 import { useRoomStore } from '@/hooks/useRoomStore'
 import { type ComponentProps } from 'react'
@@ -7,11 +8,12 @@ function Title (props: ComponentProps<'h2'>) {
 }
 
 export function DebugView () {
+  const { sessionId } = useAuthStore()
   const playerState = usePlayerStateStore()
   const room = useRoomStore()
 
   return (
-    <div className="grid grid-cols-2 gap-2 text-xs">
+    <div className="grid grid-cols-3 gap-2 text-xs">
       <div>
         <Title>playerState</Title>
         <pre>{JSON.stringify(playerState, null, 2)}</pre>
@@ -20,6 +22,11 @@ export function DebugView () {
       <div>
         <Title>room</Title>
         <pre>{JSON.stringify(room, null, 2)}</pre>
+      </div>
+
+      <div>
+        <Title>sessionId</Title>
+        <pre>{JSON.stringify({ sessionId }, null, 2)}</pre>
       </div>
     </div>
   )
