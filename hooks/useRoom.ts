bbff3efb9e3bc48a5e85ce3a5a67391939ccb50a
ngseke <ngseke@gmail.com>
2024-04-29 23:10:42 +0800

    feat: fix router

--- hooks/useRoom.ts
+++ hooks/useRoom.ts
@@ -1,3 +1,4 @@
+import { useRouter } from 'next/navigation'
 import { useAxios } from './useAxios'
 import { useRoomStore } from './useRoomStore'
 
@@ -5,9 +6,11 @@ export function useRoomActions () {
   const room = useRoomStore()
   const roomId = room?.id
   const axios = useAxios()
+  const router = useRouter()
 
   async function exitRoom () {
     await axios.post('/api/room/exit')
+    router.replace('/')
   }
 
   async function createNewBoard () {
