ac5b1aed1d47ac9dbcc22558ee57f08b0cf541d7
ngseke <ngseke@gmail.com>
2024-05-04 02:39:16 +0800

    feat: set emoji randomly

--- components/PlayerProfileForm.tsx
+++ components/PlayerProfileForm.tsx
@@ -8,6 +8,9 @@ import { Button } from './Button'
 import { useAxios } from '@/hooks/useAxios'
 import { EmojiPicker } from './EmojiPicker'
 import { playerNameMaxLength } from '@/modules/constants'
+import { faShuffle } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { random } from 'node-emoji'
 
 interface Inputs {
   name: string | null
@@ -42,6 +45,11 @@ export function PlayerProfileForm ({ onFinish }: {
     onFinish?.()
   }
 
+  function setEmojiRandomly () {
+    const { emoji } = random()
+    setValue('emoji', emoji)
+  }
+
   return (
     <form
       className="flex gap-6 py-2 sm:flex-wrap"
@@ -59,11 +67,18 @@ export function PlayerProfileForm ({ onFinish }: {
           maxLength={playerNameMaxLength}
           {...register('name', { required: true })}
         />
-        <Input
-          disabled
-          label="Avatar"
-          {...register('emoji', { required: true })}
-        />
+        <div className="grid grid-cols-[1fr_auto] items-end gap-2">
+          <Input
+            disabled
+            label="Avatar"
+            {...register('emoji', { required: true })}
+          />
+          <Button
+            icon={<FontAwesomeIcon icon={faShuffle} />}
+            onClick={setEmojiRandomly}
+          />
+        </div>
+
         <EmojiPicker
           value={watch('emoji')}
           onChange={(emoji) => { setValue('emoji', emoji) }}
