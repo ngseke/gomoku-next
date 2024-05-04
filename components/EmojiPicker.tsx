82a3a4999f7c0f813c43cce1c3a1ca8b7a947800
ngseke <ngseke@gmail.com>
2024-05-04 10:46:41 +0800

    feat: create emoji finder

--- components/EmojiPicker.tsx
+++ components/EmojiPicker.tsx
@@ -3,37 +3,84 @@
 import { cn } from '@/modules/cn'
 import { type Nullish } from '@/types/Nullish'
 import dataByGroup from 'unicode-emoji-json/data-by-group.json'
+import { Input } from './Input'
+import { random, search } from 'node-emoji'
+import { useId, useState } from 'react'
+import { Button } from './Button'
+import { faShuffle } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+function List ({ emojis, value, onChange }: {
+  emojis: Array<{ name: string, emoji: string }>
+  value?: Nullish<string>
+  onChange?: (value: string) => void
+}) {
+  return (
+    <div className="grid grid-cols-10">
+      {emojis
+        .map((emoji) => (
+          <button
+            key={emoji.name}
+            className={cn('inline-flex aspect-square items-center justify-center overflow-hidden rounded-[25%] text-3xl', {
+              'bg-neutral-300/90 dark:bg-neutral-700/90': value === emoji.emoji,
+            })}
+            type="button"
+            onClick={() => {
+              onChange?.(emoji.emoji)
+            }}
+          >{emoji.emoji}</button>
+        ))}
+    </div>
+  )
+}
 
 export function EmojiPicker ({ value, onChange }: {
   value?: Nullish<string>
   onChange?: (value: string) => void
 }) {
+  const [keyword, setKeyword] = useState('')
+
+  const shouldShowResult = Boolean(keyword.trim())
+  const filteredEmojis = keyword ? search(keyword.trim().toLowerCase()) : []
+
+  function setEmojiRandomly () {
+    const { emoji } = random()
+    onChange?.(emoji)
+  }
+
   return (
-    <div className="max-h-44 overflow-auto">
-      {dataByGroup.map((group) => (
-        <section key={group.name} className="mb-4">
-          <h2 className="mb-1 text-sm font-medium text-neutral-600 dark:text-neutral-400">{group.name}</h2>
+    <div className="flex flex-col gap-3">
+      <div className="grid grid-cols-[1fr_auto] items-end gap-2">
+        <Input
+          label="Emoji"
+          placeholder="Search..."
+          value={keyword}
+          onChange={(event) => { setKeyword(event.target.value) }}
+        />
+        <Button
+          icon={<FontAwesomeIcon icon={faShuffle} />}
+          onClick={setEmojiRandomly}
+        />
+      </div>
 
-          <div className="grid grid-cols-[repeat(auto-fit,minmax(2rem,1fr))]">
-            {group.emojis
-              .filter(emoji => Number(emoji.emoji_version))
-              .map((emoji) => (
-                <button
-                  key={emoji.slug}
-                  className={cn('inline-flex aspect-square items-center justify-center overflow-hidden rounded-[25%] text-3xl', {
-                    'bg-neutral-300/90 dark:bg-neutral-700/90': value === emoji.emoji,
-                  })}
-                  type="button"
-                  onClick={() => {
-                    onChange?.(emoji.emoji)
-                  }}
-                >{emoji.emoji}</button>
-              ))}
-          </div>
-        </section>
-      ))}
-
+      <div className="h-48 max-h-48 overflow-auto">
+        {shouldShowResult
+          ? <List
+              emojis={filteredEmojis}
+              value={value}
+              onChange={onChange}
+            />
+          : dataByGroup.map((group) => (
+            <section key={group.name} className="mb-4">
+              <h2 className="mb-1 text-sm font-bold text-neutral-600 dark:text-neutral-400">{group.name}</h2>
+              <List
+                emojis={group.emojis}
+                value={value}
+                onChange={onChange}
+              />
+            </section>
+          ))}
+      </div>
     </div>
-
   )
 }
--- components/PlayerProfileForm.tsx
+++ components/PlayerProfileForm.tsx
@@ -8,9 +8,6 @@ import { Button } from './Button'
 import { useAxios } from '@/hooks/useAxios'
 import { EmojiPicker } from './EmojiPicker'
 import { playerNameMaxLength } from '@/modules/constants'
-import { faShuffle } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { random } from 'node-emoji'
 import { usePlayer } from '@/hooks/usePlayer'
 
 interface Inputs {
@@ -43,11 +40,6 @@ export function PlayerProfileForm ({ onFinish }: {
     }
   }
 
-  function setEmojiRandomly () {
-    const { emoji } = random()
-    setValue('emoji', emoji)
-  }
-
   return (
     <form
       className="flex gap-6 py-2 sm:flex-wrap"
@@ -65,17 +57,6 @@ export function PlayerProfileForm ({ onFinish }: {
           maxLength={playerNameMaxLength}
           {...register('name', { required: true })}
         />
-        <div className="grid grid-cols-[1fr_auto] items-end gap-2">
-          <Input
-            disabled
-            label="Avatar"
-            {...register('emoji', { required: true })}
-          />
-          <Button
-            icon={<FontAwesomeIcon icon={faShuffle} />}
-            onClick={setEmojiRandomly}
-          />
-        </div>
 
         <EmojiPicker
           value={watch('emoji')}
