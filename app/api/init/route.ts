5285c422ac173c15e86aacc13882eff924f398b1
ngseke <ngseke@gmail.com>
2024-05-04 15:18:46 +0800

    feat: delete anonymous users

--- app/api/init/route.ts
+++ app/api/init/route.ts
@@ -1,15 +1,25 @@
-import { firebaseAdminDatabase } from '@/modules/firebaseAdmin/firebaseAdmin'
-
-const database = {
-  players: {},
-  playerStates: {},
-  rooms: {},
-  boards: {},
-  chats: {},
-}
+import { firebaseAdminAuth, firebaseAdminDatabase } from '@/modules/firebaseAdmin/firebaseAdmin'
 
 export async function POST () {
-  await firebaseAdminDatabase.ref().set(database)
+  await firebaseAdminDatabase.ref().set({})
+  await deleteAnonymousUsers()
 
   return new Response(null, { status: 204 })
+}
+
+async function deleteAnonymousUsers (nextPageToken?: string) {
+  const { users, pageToken } =
+    await firebaseAdminAuth.listUsers(20, nextPageToken)
+
+  await Promise.allSettled(users.map(async (user) => {
+    const isAnonymous = (user.providerData.length === 0)
+    if (isAnonymous) {
+      await firebaseAdminAuth.deleteUser(user.uid)
+      console.log(`\`${user.uid}\` deleted.`)
+    }
+  }))
+
+  if (pageToken) {
+    await deleteAnonymousUsers(pageToken)
+  }
 }
