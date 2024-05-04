d0fe8f677cd1591ebac8404ac99a93d7a1c94e68
ngseke <ngseke@gmail.com>
2024-05-04 15:24:21 +0800

    refactor: simplify exit room api

--- app/api/room/exit/route.ts
+++ app/api/room/exit/route.ts
@@ -1,20 +1,11 @@
 import { exitRoom } from '@/modules/firebaseAdmin/exitRoom'
-import { fetchPlayer } from '@/modules/firebaseAdmin/fetchPlayer'
-import { parseSessionId } from '@/modules/firebaseAdmin/parseSessionId'
+import { parseAuthorization } from '@/modules/firebaseAdmin/parseAuthorization'
 
 export async function POST (
   request: Request,
 ) {
-  const player = await fetchPlayer(request)
-  if (!player) return Response.json(null, { status: 403 })
-
-  const sessionId = parseSessionId(request)
-  if (!sessionId) {
-    return Response.json(
-      'Missing header `X-Session-Id`',
-      { status: 400 }
-    )
-  }
+  const auth = await parseAuthorization(request)
+  if (!auth) return Response.json(null, { status: 403 })
 
   await exitRoom(request)
   return new Response(null, { status: 204 })
