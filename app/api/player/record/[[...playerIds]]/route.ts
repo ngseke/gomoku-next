3c78d999de7e93fa322ea69457493cbb9785eec2
ngseke <ngseke@gmail.com>
2024-05-11 16:29:06 +0800

    feat: add params to get player record api

--- app/api/player/record/[[...playerIds]]/route.ts
+++ app/api/player/record/[[...playerIds]]/route.ts
@@ -2,11 +2,16 @@ import { parseAuthorization } from '@/modules/firebaseAdmin/parseAuthorization'
 import { getPlayerRecordRef } from '@/modules/firebaseAdmin/refs'
 import { type PlayerRecord } from '@/types/PlayerRecord'
 
-export async function GET (request: Request) {
+export async function GET (
+  request: Request,
+  { params }: { params: { playerIds: string[] } }
+) {
   const auth = await parseAuthorization(request)
   if (!auth) return Response.json(null, { status: 403 })
 
-  const ref = getPlayerRecordRef(auth.uid)
+  const playerId = params.playerIds?.[0] ?? auth.uid
+
+  const ref = getPlayerRecordRef(playerId)
   const record = (await ref.get()).val() as PlayerRecord
 
   return Response.json(record)
