9bbb727027affceaf3dfc477745d432430602bc9
ngseke <ngseke@gmail.com>
2024-05-12 14:09:53 +0800

    feat: add i18n

--- components/NotCurrentSessionDialog.tsx
+++ components/NotCurrentSessionDialog.tsx
@@ -3,8 +3,11 @@ import { Dialog } from './Dialog'
 import { Button } from './Button'
 import { useCreateOrJoinRoom } from '@/hooks/useCreateOrJoinRoom'
 import { usePlayerStateStore } from '@/hooks/usePlayerStateStore'
+import { useTranslations } from 'next-intl'
 
 export function NotCurrentSessionDialog (props: ComponentProps<typeof Dialog>) {
+  const t = useTranslations()
+
   const { joinRoom, isCreatingOrJoiningRoom } = useCreateOrJoinRoom()
 
   const playerState = usePlayerStateStore()
@@ -19,12 +22,14 @@ export function NotCurrentSessionDialog (props: ComponentProps<typeof Dialog>) {
     <Dialog {...props}>
       <div className="flex flex-col gap-4">
         <p>
-          You have another session open elsewhere. Click the button to resume the session here.
+          {t('message.notCurrentSession')}
         </p>
         <Button
           loading={isCreatingOrJoiningRoom}
           onClick={handleClickResume}
-        >Resume</Button>
+        >
+          {t('action.resume')}
+        </Button>
       </div>
     </Dialog>
   )
